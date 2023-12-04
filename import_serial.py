be96a01ce78aa1ce5ee5c1b092de5f00353758d2
Aidan Zinn <aidanzinn@gmail.com>
2023-12-04 14:44:58 -0800

    Fixed port

--- import_serial.py
+++ import_serial.py
@@ -2,10 +2,46 @@ import serial
 import time
 from datetime import datetime
 
-# Set up the serial line
-port = 'COM5'
-ser = serial.Serial(port, 115200)
-ser.flushInput()
+# # Set up the serial line
+# port = 'COM5'
+# ser = serial.Serial(port, 115200)
+# ser.flushInput()
+
+# List of ports available
+ports = serial.tools.list_ports.comports()  
+
+# Opens the class, not the port, and fill the class below
+ser = serial.Serial()    
+
+# Create an empty list to store the ports 
+portsList = []
+
+for onePort in ports:               # loop through ports list and save them to the port list
+    portsList.append(str(onePort))  # Add the ports into the list 
+    print(str(onePort))             # Print them to view them
+
+
+# Ask the user to declare which Port number to use
+val = input("Select Port: COM")
+
+for x in range(0, len(portsList)):                  
+    if portsList[x].startswith("COM" + str(val)):   
+        portVar = "COM" + str(val)                  
+        print(f"Selected Port: {portVar}") 
+        # Ask the user to declare the baud rate 
+        baud = input("Set baud rate (9600,115200):")  
+        # set baud
+        ser.baudrate = baud
+        # set port 
+        ser.port = portVar
+        break    
+    else: 
+        print('\nThe port selected is not a valid input. The program will terminate.\n')
+        ser.close()
+
+# Open serial
+ser.open()
+ser.reset_input_buffer()
 
 # Function to create a new file with a timestamp in the name
 def create_new_file():
