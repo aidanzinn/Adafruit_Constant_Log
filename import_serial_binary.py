a346bd74052830610787100d582a6b21c10ae8b6
Aidan Zinn <aidanzinn@gmail.com>
2023-12-04 15:20:09 -0800

    modified binary to run on linux

--- import_serial_binary.py
+++ import_serial_binary.py
@@ -3,46 +3,10 @@ import time
 from datetime import datetime
 import struct
 
-# # Set up the serial line
-# port = 'COM5'
-# ser = serial.Serial(port, 115200)
-# ser.flushInput()
-
-# List of ports available
-ports = serial.tools.list_ports.comports()  
-
-# Opens the class, not the port, and fill the class below
-ser = serial.Serial()    
-
-# Create an empty list to store the ports 
-portsList = []
-
-for onePort in ports:               # loop through ports list and save them to the port list
-    portsList.append(str(onePort))  # Add the ports into the list 
-    print(str(onePort))             # Print them to view them
-
-
-# Ask the user to declare which Port number to use
-val = input("Select Port: COM")
-
-for x in range(0, len(portsList)):                  
-    if portsList[x].startswith("COM" + str(val)):   
-        portVar = "COM" + str(val)                  
-        print(f"Selected Port: {portVar}") 
-        # Ask the user to declare the baud rate 
-        baud = input("Set baud rate (9600,115200):")  
-        # set baud
-        ser.baudrate = baud
-        # set port 
-        ser.port = portVar
-        break    
-    else: 
-        print('\nThe port selected is not a valid input. The program will terminate.\n')
-        ser.close()
-
-# Open serial
-ser.open()
-ser.reset_input_buffer()
+# Set up the serial line
+port = '/dev/ttyACM0'
+ser = serial.Serial(port, 115200)
+ser.flushInput()
 
 # Function to create a new file with a timestamp in the name
 def create_new_file():
@@ -52,6 +16,7 @@ def create_new_file():
 
 # Initialize variables
 file = create_new_file()
+line_count = 0
 byte_count = 0
 while True:
     try:
@@ -68,7 +33,13 @@ while True:
 
 
             file.write(ser_bytes)
-            file.flush() 
+            file.flush()
+            line_count += 1
+
+        if line_count >= 1000000:
+            file.close()  # Close the current file
+            file = create_new_file()  # Open a new file with a new timestamp
+            line_count = 0  # Reset line count 
 
     except KeyboardInterrupt:
         print("Keyboard Interrupt: Exiting...")
